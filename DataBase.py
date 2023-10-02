9e1a6f59c38dcae1527b5710d45eb86d853eab66
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2023-10-02 12:04:44 -0300

    Função insert_dataset para inserir dados de uma planilha csv

--- DataBase.py
+++ DataBase.py
@@ -39,12 +39,16 @@ class SQLdb:
                              PubMed_ID, PDB, AlphaFoldDB, Pfam))
         self.connection.commit()
 
-    def insert_dataset(self, list_):
-        for i in list_:
-            self.insert_data(i['entry'], i['entry_name'], i['Protein_Names'],
-                             i['Gene_Names'], i['Organism'], i['Length'],
-                             i['EC_Number'], i['PubMed_ID'], i['PDB'],
-                             i['AlphaFoldDB'], i['Pfam'])
+    def insert_dataset(self, list_strings):
+        for cliente_str in list_strings:
+            cliente_data = cliente_str.split(',')
+            if len(cliente_data) == 11:
+                entry, entry_name, Protein_Names, Gene_Names, Organism, Length, EC_Number, PubMed_ID, PDB, AlphaFoldDB, Pfam = cliente_data
+                self.insert_data(entry.strip(), entry_name.strip(),
+                                 Protein_Names.strip(), Gene_Names.strip(),
+                                 Organism.strip(), Length.strip(),
+                                 EC_Number.strip(), PubMed_ID.strip(),
+                                 PDB.strip(), AlphaFoldDB.strip(), Pfam.strip())
 
     def remover_cliente(self, entry_id):
         self.cursor.execute("DELETE FROM clientes WHERE id=?", (entry_id,))
