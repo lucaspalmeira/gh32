924ada7ce75d529a398f1be2904536d49bc86778
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2023-10-03 17:11:04 -0300

    Função insert_dataset para inserir dados de uma planilha csv

--- DataBase.py
+++ DataBase.py
@@ -40,28 +40,30 @@ class SQLdb:
         self.connection.commit()
 
     def insert_dataset(self, list_strings):
-        for cliente_str in list_strings:
-            cliente_data = cliente_str.split(',')
-            if len(cliente_data) == 11:
-                entry, entry_name, Protein_Names, Gene_Names, Organism, Length, EC_Number, PubMed_ID, PDB, AlphaFoldDB, Pfam = cliente_data
+        for _str in list_strings:
+            _data = _str.split(',')
+            if len(_data) == 11:
+                (entry, entry_name, Protein_Names, Gene_Names, Organism,
+                 Length, EC_Number, PubMed_ID, PDB, AlphaFoldDB, Pfam) = _data
                 self.insert_data(entry.strip(), entry_name.strip(),
                                  Protein_Names.strip(), Gene_Names.strip(),
                                  Organism.strip(), Length.strip(),
                                  EC_Number.strip(), PubMed_ID.strip(),
                                  PDB.strip(), AlphaFoldDB.strip(), Pfam.strip())
 
-    def remover_cliente(self, entry_id):
-        self.cursor.execute("DELETE FROM clientes WHERE id=?", (entry_id,))
+    def remover_entry(self, entry_id):
+        self.cursor.execute("DELETE FROM inulinases WHERE id=?", (entry_id,))
         self.connection.commit()
 
     def close_connection(self):
         self.connection.close()
 
 
-# Exemplo de uso da classe
+"""
 if __name__ == "__main__":
     db = SQLdb('inulinases.db')
     db.create_table()
     db.insert_data()
     db.insert_dataset()
     db.close_connection()
+"""
