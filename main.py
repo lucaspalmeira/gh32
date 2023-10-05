afd760b4556e2c7d92229b4c88896de3b6e0a652
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2023-10-05 10:39:14 -0300

    Função criada para limpar os dados

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ from DataBase import SQLdb
 
 #execute.create_table('inulinases.db')
 
-with open('inulinases.csv', 'r') as file:
-    lines = file.readlines()
-    for i in lines:
-        x = i.rstrip('\n').split(',')
-        if '' in x:
-            print(x)
+def clean_csv(file):
+    list_strings = []
+    with open(file, 'r') as file:
+        lines = file.readlines()
+        for i in lines:
+            x = i.rstrip('\n').split(',')
+            x = [item for item in x if item != '']
+            x = ', '.join(x)
+            list_strings.append(x)
+
+    return list_strings
+
+
+print(clean_csv('inulinases.csv'))
+
 #execute.insert_dataset()
