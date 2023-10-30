adc5647265c7dead4c1b2f98163ec439a7ece28b
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2023-10-30 14:58:20 -0300

    Função para ler um arquivo csv e retornar uma lista de strings

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from DataBase import SQLdb
+import pandas as pd
 
 
-#execute = SQLdb
+execute = SQLdb
 
-#execute.create_table('inulinases.db')
+execute.create_table('inulinases.db')
+def read_csv(file):
 
-def clean_csv(file):
-    list_strings = []
-    with open(file, 'r') as file:
-        lines = file.readlines()
-        for i in lines:
-            x = i.rstrip('\n').split(',')
-            x = [item for item in x if item != '']
-            x = ', '.join(x)
-            list_strings.append(x)
-
-    return list_strings
+    list_string = []
 
 
-print(clean_csv('inulinases.csv'))
+    df = pd.read_csv(file)
+    df = df[['Entry', 'Entry Name', 'Protein names', 'Gene Names', 'Organism',
+    'Length', 'EC number', 'PubMed ID', 'PDB', 'AlphaFoldDB', 'Pfam']]
 
-#execute.insert_dataset()
+    for i, row in df.iterrows():
+        x = (f'{row["Entry"]}, {row["Entry Name"]}, '
+             f'{row["Protein names"]}, {row["Gene Names"]}, '
+             f'{row["Organism"]}, {row["Length"]}, {row["EC number"]}, '
+             f'{row["PubMed ID"]}, {row["PDB"]}, {row["AlphaFoldDB"]}, '
+             f'{row["Pfam"]}')
+        print(x)
+        list_string.append(x)
+
+    for x in list_string:
+        print(x)
+
+    return list_string
+
+read_csv('inulinases.csv')
+
+execute.insert_dataset()
