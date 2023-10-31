4ea3764e761934eb668c717f91d0e97fea60b066
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2023-10-31 10:09:58 -0300

    Correção de sintaxe

--- DataBase.py
+++ DataBase.py
@@ -1,9 +1,8 @@
 import sqlite3
 
-
 class SQLdb:
-    def __init__(self, name_bank):
-        self.connection = sqlite3.connect(name_bank)
+    def __init__(self):
+        self.connection = sqlite3.connect('inulinases.db')
         self.cursor = self.connection.cursor()
 
     def create_table(self):
@@ -13,14 +12,14 @@ class SQLdb:
                 Entry TEXT NOT NULL,
                 Entry_Name TEXT NOT NULL,
                 Protein_Names TEXT NOT NULL,
-                Gene Names,
-                Organism NOT NULL,
-                Length NOT NULL,
-                EC_Number,
-                PubMed_ID,
-                PDB,
-                AlphaFoldDB,
-                Pfam
+                Gene_Names TEXT,  -- Specify the data type for 'Gene Names'
+                Organism TEXT NOT NULL,
+                Length TEXT NOT NULL,
+                EC_Number TEXT,  -- Allow null values for 'EC_Number'
+                PubMed_ID TEXT,  -- Allow null values for 'PubMed_ID'
+                PDB TEXT,
+                AlphaFoldDB TEXT,
+                Pfam TEXT
             )
         ''')
         self.connection.commit()
@@ -51,19 +50,20 @@ class SQLdb:
                                  EC_Number.strip(), PubMed_ID.strip(),
                                  PDB.strip(), AlphaFoldDB.strip(), Pfam.strip())
 
-    def remover_entry(self, entry_id):
+    def remove_entry(self, entry_id):
         self.cursor.execute("DELETE FROM inulinases WHERE id=?", (entry_id,))
         self.connection.commit()
 
     def close_connection(self):
         self.connection.close()
 
-
 """
 if __name__ == "__main__":
-    db = SQLdb('inulinases.db')
+    db = SQLdb()
     db.create_table()
-    db.insert_data()
-    db.insert_dataset()
+    # You need to provide data to insert_data and insert_dataset.
+    # Example:
+    # db.insert_data("value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8", "value9", "value10", "value11")
+    # db.insert_dataset(["value1,value2,value3,value4,value5,value6,value7,value8,value9,value10,value11"])
     db.close_connection()
 """
