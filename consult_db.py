2ea6d84509c8a47edaa8196bd949a1dbb962072d
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2023-11-06 15:16:11 -0300

    Elaborada consulta ao banco com base no index de uma coluna

--- consult_db.py
+++ consult_db.py
@@ -3,4 +3,8 @@ from DataBase import SQLdb
 
 if __name__ == '__main__':
     execute = SQLdb()
-    execute.consult_select()
+    #execute.consult_select()
+    column_index = 3  # Índice da terceira coluna (começando em 0)
+    values = execute.select_all_by_column_index(column_index)
+    for i in values:
+        print(i)
