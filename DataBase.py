e04c65cba80a5a120a7725d6918ece0bbc49f800
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2023-11-06 15:35:39 -0300

    Construção da classe MONGOdb

--- DataBase.py
+++ DataBase.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pymongo
 
 class SQLdb:
     def __init__(self):
@@ -68,6 +69,81 @@ class SQLdb:
 
     def close_connection(self):
         self.connection.close()
+
+
+class MONGOdb:
+    def __int__(self):
+        def __init__(self, entry, entry_name, protein_names, gene_names,
+                     organism, length, ec_number, pubmed_id, pdb,
+                     alphafold_db, pfam):
+            self.entry = entry
+            self.entry_name = entry_name
+            self.protein_names = protein_names
+            self.gene_names = gene_names
+            self.organism = organism
+            self.length = length
+            self.ec_number = ec_number
+            self.pubmed_id = pubmed_id
+            self.pdb = pdb
+            self.alphafold_db = alphafold_db
+            self.pfam = pfam
+
+        def to_dict(self):
+            return {
+                "entry": self.entry,
+                "entry_name": self.entry_name,
+                "protein_names": self.protein_names,
+                "gene_names": self.gene_names,
+                "organism": self.organism,
+                "length": self.length,
+                "ec_number": self.ec_number,
+                "pubmed_id": self.pubmed_id,
+                "pdb": self.pdb,
+                "alphafold_db": self.alphafold_db,
+                "pfam": self.pfam
+            }
+
+        def __str__(self):
+            return f"ProteinEntry(entry={self.entry}, entry_name={self.entry_name}, organism={self.organism})"
+
+# Conecte-se ao servidor MongoDB (por padrão, ele se conectará ao servidor local)
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+# Crie um banco de dados (se não existir)
+db = client["proteins_database"]
+
+# Crie uma coleção para armazenar as informações de proteínas
+collection = db["protein_entries"]
+
+# Crie uma instância da classe ProteinEntry com os dados
+protein = ProteinEntry(
+    entry="P12345",
+    entry_name="ProteinA",
+    protein_names=["Protein A"],
+    gene_names=["Gene A"],
+    organism="Example Organism",
+    length=300,
+    ec_number="2.1.1.1",
+    pubmed_id="12345",
+    pdb="PDB123",
+    alphafold_db="AlphaFoldDB",
+    pfam="PF12345"
+)
+
+# Insira os dados no banco de dados
+entry_data = protein.to_dict()
+insertion_result = collection.insert_one(entry_data)
+
+print("ID do documento inserido:", insertion_result.inserted_id)
+
+# Consultar o banco de dados
+result = collection.find_one({"entry": "P12345"})
+if result:
+    retrieved_protein = ProteinEntry(**result)
+    print("Proteína recuperada:", retrieved_protein)
+
+# Feche a conexão com o servidor MongoDB
+client.close()
 
 
 if __name__ == "__main__":
