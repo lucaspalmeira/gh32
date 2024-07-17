766c21979cfc42f65bb83ea2a074c6e972abbfc7
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2024-07-17 15:09:53 -0300

    Incluíndo o diretório base (BASE_DIR)

--- rungh32.sh
+++ rungh32.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Diretório base
+
+BASE_DIR=$(dirname $(realpath $0))
+
 # Executar a criação/atualização do banco
 
-python create_db.py
+python $BASE_DIR/create_db.py
 
 # Executar o cálculo de classificação enzimática
 
-python /CLEAN/app/CLEAN_infer_fasta.py gh32
+python $BASE_DIR/CLEAN/app/CLEAN_infer_fasta.py gh32
 
 # Executar filtragem dos dados de EC e adicioná-los ao banco
 
-python ec_clean.py
+python $BASE_DIR/ec_clean.py
