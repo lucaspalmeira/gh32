24ac7ed28120089e179a73d62ed2055ad1551080
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2024-08-14 12:32:47 -0300

    update

--- runpipeline.sh
+++ runpipeline.sh
@@ -6,7 +6,7 @@ docker run -it \
   -v $(pwd):/root/.cache/torch/hub/checkpoints \
   -v $(pwd)/CLEAN/app/data/inputs:/app/data/inputs \
   -v $(pwd)/results:/app/results/inputs \
-  moleculemaker/clean-image-amd64 \
+  clean-image \
   /bin/bash -c 'echo Starting Execution && python /app/CLEAN_infer_fasta.py --fasta_data gh32'
 
 python ec_clean.py
