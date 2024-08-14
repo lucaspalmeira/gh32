ca12891082945cc999fcc72d08c0d62af1340211
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2024-08-13 23:55:35 -0300

    shell script para execução do pipeline

---
+++ runpipeline.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python create_db.py
+
+docker run -it \
+  -v $(pwd):/root/.cache/torch/hub/checkpoints \
+  -v $(pwd)/CLEAN/app/data/inputs:/app/data/inputs \
+  -v $(pwd)/results:/app/results/inputs \
+  moleculemaker/clean-image-amd64 \
+  /bin/bash -c 'echo Starting Execution && python /app/CLEAN_infer_fasta.py --fasta_data gh32'
+
+python ec_clean.py
