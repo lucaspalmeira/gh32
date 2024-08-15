46b2ae7ace517521577139a7bb03532470ed0c8b
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2024-08-15 14:29:29 -0300

    Uso de ponto e vírgula para forçar a execução mesmo quando a saída do sehll for 1

--- runpipeline.sh
+++ runpipeline.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# uso do operador ; para executar o código mesmo que o código de saída anterior seja 1
 python create_db.py; python CLEAN/app/CLEAN_infer_fasta.py --fasta_data gh32; python ec_clean.py
