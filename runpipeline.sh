4c9651a861c5d321a6bece4943d48a0203354259
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2024-08-15 14:27:54 -0300

    uso de ponto e vírgula para forçar a execução mesmo quando a saída do sehll for 1

--- runpipeline.sh
+++ runpipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python create_db.py && python CLEAN/app/CLEAN_infer_fasta.py --fasta_data gh32 && python ec_clean.py
+python create_db.py; python CLEAN/app/CLEAN_infer_fasta.py --fasta_data gh32; python ec_clean.py
