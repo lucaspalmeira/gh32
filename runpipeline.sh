35c0187a7f3ba187b64fa81294f971d131b1882c
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2024-08-16 16:23:39 -0300

    ajuste na flag -d

--- runpipeline.sh
+++ runpipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -it --name mongodb mongo -d
+docker run -d -it --name mongodb mongo
 
 docker run -it --name gh32-pipeline -v $(pwd):/gh32_data_results GH32 /bin/bash -c 'python /app/create_db.py'
