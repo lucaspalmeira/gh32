7ef6399327d73444f1d8ed89928cdac7e2412c28
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2024-08-16 15:07:28 -0300

    Reorgranizando a estrutura do projeto

--- runpipeline.sh
+++ runpipeline.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# uso do operador ; para executar o código mesmo que o código de saída anterior seja 1
-python create_db.py; python CLEAN/app/CLEAN_infer_fasta.py --fasta_data gh32; python ec_clean.py
+python create_db.py
