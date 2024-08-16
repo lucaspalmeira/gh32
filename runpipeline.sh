ece3d517bd53eb806e1ed6cf81e98f9e363db6c1
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2024-08-16 16:29:01 -0300

    correção do path

--- runpipeline.sh
+++ runpipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --name gh32-pipeline -v $(pwd):/gh32_data_results GH32 /bin/bash -c 'python /app/create_db.py'
+docker run -it --name gh32-pipeline -v $(pwd):/gh32_data_results gh32 /bin/bash -c 'python /gh32_data_results/app/create_db.py'
