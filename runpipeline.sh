59d553c57cf18fa313f8350ec60c50f8c6313b9b
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2024-08-17 22:59:45 -0300

    adicionando a rede gh32-network

--- runpipeline.sh
+++ runpipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -v $(pwd):/gh32_data_results gh32-pipeline /bin/bash -c 'python /gh32_data_results/app/create_db.py'
+docker run -it --network gh32-network -v $(pwd):/gh32_data_results gh32-pipeline /bin/bash -c 'python /gh32_data_results/app/create_db.py'
