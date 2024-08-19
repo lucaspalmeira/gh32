63457a531c976ffddf5f283a60394e05782198ee
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2024-08-19 12:42:46 -0300

    substituí /gh32_data_results/CLEAN_infer_fasta.py por $(pwd)/CLEAN_infer_fasta.py

--- runpipeline.sh
+++ runpipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run -it --network gh32-network -v $(pwd):/gh32_data_results gh32-pipeline /bin/bash -c 'python /gh32_data_results/app/create_db.py'; \
-docker run -it --network gh32-network -v $(pwd):/gh32_data_results moleculemaker/clean-image-amd64 /bin/bash -c 'echo Starting Execution && python /gh32_data_results/CLEAN_infer_fasta.py --fasta_data /gh32_data_results/gh32'; \
+docker run -it --network gh32-network -v $(pwd):/gh32_data_results moleculemaker/clean-image-amd64 /bin/bash -c 'echo Starting Execution && python $(pwd)/CLEAN_infer_fasta.py --fasta_data /gh32_data_results/gh32'; \
 docker run -it --network gh32-network -v $(pwd):/gh32_data_results gh32-pipeline /bin/bash -c 'python /gh32_data_results/app/ec_clean.py'
 
 echo "Concluído."
