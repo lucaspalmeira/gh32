873601a8ba61a4d29a7b8d2037618eb0187ae543
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2024-08-21 15:49:42 -0300

    update

--- runpipeline.sh
+++ runpipeline.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 docker run -it --network gh32-network -v $(pwd):/gh32 gh32-pipeline /bin/bash -c 'python /gh32/app/create_db.py'; \
-docker run -it --network gh32-network -v $(pwd):/gh32 moleculemaker/clean-image-amd64 /bin/bash -c 'echo Starting Execution && python $(pwd)/CLEAN_infer_fasta.py --fasta_data gh32/app/gh32'; \
+cd /gh32/app; \
+docker run -it -v $(pwd):/app/data/inputs moleculemaker/clean-image-amd64 /bin/bash -c 'echo Starting Execution && python $(pwd)/CLEAN_infer_fasta.py --fasta_data gh32'; \
+cd ..; \
 docker run -it --network gh32-network -v $(pwd):/gh32 gh32-pipeline /bin/bash -c 'python /gh32/app/ec_clean.py'
 
 echo "Concluído."
