caebc82df93f51f24d22a076b0ca6e7281e10a6b
lucaspalmeira <lucaspalmeira.1945@gmail.com>
2024-08-22 10:52:57 -0300

    novo volume para obter os resultados do CLEAN

--- runpipeline.sh
+++ runpipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run -it --network gh32-network -v $(pwd):/gh32 gh32-pipeline /bin/bash -c 'python /gh32/app/create_db.py'; \
-cd app; docker run -it -v $(pwd):/app/data/inputs moleculemaker/clean-image-amd64 /bin/bash -c 'echo Starting Execution && python $(pwd)/CLEAN_infer_fasta.py --fasta_data gh32'; \
+cd app; docker run -it -v $(pwd):/app/data/inputs -v $(pwd)/results/inputs:/app/results/inputs/ moleculemaker/clean-image-amd64 /bin/bash -c 'echo Starting Execution && python $(pwd)/CLEAN_infer_fasta.py --fasta_data gh32'; \
 cd ..; docker run -it --network gh32-network -v $(pwd):/gh32 gh32-pipeline /bin/bash -c 'python /gh32/app/ec_clean.py'
 
 echo "Concluído."
